86a463444347d193531595cd7b6c030f4c29c116
vitor poerschke <vitordrakomanico@gmail.com>
2025-06-18 16:50:01 -0300

    fix: concertado routes de usuario

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 const autenticarToken = require('../middleware/auth');
 
-// Listar usuários (Protegido)
-router.get('/', autenticarToken, usuarioController.listarUsuarios);
+// Proteger todas as rotas de usuários
+router.use(autenticarToken);
 
-// Criar novo usuário (Protegido)
-router.post('/', autenticarToken, usuarioController.criarUsuario);
-
-// Deletar usuário por ID (Protegido)
-router.delete('/:id', autenticarToken, usuarioController.deletarUsuario);
+// Rotas CRUD de Usuários
+router.get('/', usuarioController.listarUsuarios);
+router.get('/buscar/:id', usuarioController.buscarUsuarioPorId);
+router.post('/', usuarioController.criarUsuario);
+router.put('/atualizar/:id', usuarioController.atualizarUsuario);
+router.delete('/:id', usuarioController.deletarUsuario);
 
 module.exports = router;
-// Exporta as rotas de usuários para serem usadas no index.js
